63bec793b0bfd64c98ba98b39ee75a41e8d4c7a5
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-19 09:51:07 -0300

    feat: Consulta 6. Filmes em que a pessoa diretora atuou (ou pelo menos um deles), por ordem de lançamento (mais recentes primeiro)

--- src/main/java/com/betrybe/consultafilmes/Queries.java
+++ src/main/java/com/betrybe/consultafilmes/Queries.java
@@ -9,6 +9,7 @@ import static java.util.stream.Collectors.toSet;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -103,7 +104,12 @@ public class Queries {
    * pelo menos um dos itens do campo `directors` também é um item do campo `atores`.</p>
    */
   public List<Movie> moviesWithAtLeastOneDirectorActedMostRecentFirst() {
-    return emptyList(); // TODO: Implementar.
+    return movies.stream()
+        .filter(movie -> movie.getDirectors().stream()
+          .anyMatch(director -> movie.getActors().contains(director)))
+        .distinct()
+        .sorted(Comparator.comparingInt(Movie::getReleaseYear).reversed())
+        .toList();
   }
 
   /**
