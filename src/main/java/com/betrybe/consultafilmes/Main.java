176233b61e31301c8f87a21b2e6cc9efbe6a8518
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-19 10:36:40 -0300

    class Main

--- src/main/java/com/betrybe/consultafilmes/Main.java
+++ src/main/java/com/betrybe/consultafilmes/Main.java
@@ -1,5 +1,6 @@
 package com.betrybe.consultafilmes;
 
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
@@ -13,5 +14,14 @@ public class Main {
     Queries queries = new Queries(Movies.all());
     Set<String> resultados = queries.actorsThatInterpretThemselves();
     System.out.println(resultados);
+    //    System.out.println(Movies.spaceJam2021.getActorsByCharacters());
+    //    System.out.println(Movies.spaceJam2021.getActors());
+    //    System.out.println(queries.actorsThatActedInMoviesOfDirectorInAlphabeticOrder("Mel Gibson"));
+    //    System.out.println(queries.moviesWithAtLeastOneDirectorActedMostRecentFirst());
+    //    System.out.println(queries.moviesReleasedInYearGroupedByCategory(2021));
+    //    System.out.println(queries.actorsThatInterpretThemselves());
+    //    System.out.println(queries.moviesByGivenYear(1940));
+    //    Map<String, Set<String>> resultados2 = queries.selectMoviesByActor("Leonardo DiCaprio");
+    //    System.out.println(resultados2);
   }
 }
